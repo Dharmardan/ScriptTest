86049820a33802a228b6628e564ec807fd691bbe
Ryoma.A <hallelujah.14k@icloud.com>
2017-10-30 16:36:07 +0900

    レッスン4ScriptTestプロジェクト
    ScriptTestプロジェクトの課題提出

---
+++ Assets/Test.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boss{
+	private int hp=100; //体力
+	private int power=25; //攻撃力
+	private int mp=53; //魔力
+
+	//攻撃用の関数
+	public void magic(int mag){
+		//MPを消費して攻撃
+		for(int i=0;i<=10;i++){
+			this.mp -= mag;
+			if (mp >= 0) {
+				Debug.Log ("魔法攻撃をした。残りMPは" + this.mp + "。");
+			} else {
+				Debug.Log ("MPが足りないため魔法が使えない。");
+			}
+		}
+	}
+
+
+	//防御用の関数
+	public void defence(int damage){
+		Debug.Log (damage + "のダメージを受けた");
+		//HPを減らす
+		this.hp -= damage;
+	}
+}
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+		//配列の初期化
+		int[] array={10,20,30,40,50};
+		//配列の各要素を順番に表示
+		for (int i = 0; i < array.Length; i++) {
+			Debug.Log (array [i]);
+		}
+		//配列の各要素を逆順に表示
+		for (int i = 4; i >= 0; i--) {
+			Debug.Log (array [i]);
+		}
+
+			//bossクラスの変数を宣言してインスタンスを代入
+			boss lastboss = new boss ();
+			//攻撃用の関数を呼び出す
+			lastboss.magic (5);
+			//防御用の関数を呼び出す
+			lastboss.defence (3);
+		}
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
